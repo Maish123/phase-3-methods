25374ee9653bcf07986f4f808fe6bc5d3c6569f0
Denis Maina <Denis.maina@student.moringaschool.com>
2022-08-16 08:00:56 +0300

    commit tree 2d3d8f5b0d8d1e066fc5e1bc8cfe42b7d80a8e32

--- methods.rb
+++ methods.rb
@@ -1,1 +1,37 @@
 # Your code here!
+
+#function for greeting programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#define greet with one argument,name
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+#define greet with default value for name
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default "Naureen"
+
+#define a method add that gives sum of two nums passed as arguments
+def add(num1, num2)
+    num1 + num2
+end
+add(2,2)
+
+#define a function halve, that takes a number and halves it if its an integer
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num/2
+end
+
+halve(4)
+
